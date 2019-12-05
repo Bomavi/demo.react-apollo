f8748443c0497602a0654e63d123e732a53f32ad
Bomavi <max.bozh.n1mbo@gmail.com>
2019-12-05 23:12:45 +0200

    Switch Home/TaskCheckbox to TS, add typings

--- src/features/Home/components/TaskCheckbox/index.tsx
+++ src/features/Home/components/TaskCheckbox/index.tsx
@@ -1,6 +1,5 @@
 /* npm imports: common */
 import React from 'react';
-import PropTypes from 'prop-types';
 
 /* npm imports: material-ui/core */
 import Checkbox from '@material-ui/core/Checkbox';
@@ -9,7 +8,16 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 /* local imports: common */
 import { useStyles } from './styles';
 
-const TaskCheckbox = React.memo(({ value, disabled, onChange, isFetching }) => {
+export interface TaskCheckboxProps {
+	value: boolean;
+	disabled?: boolean;
+	isFetching?: boolean;
+	onChange: () => void;
+}
+
+const TaskCheckbox: React.FC<TaskCheckboxProps> = React.memo(props => {
+	const { value, disabled, onChange, isFetching = false } = props;
+
 	const classes = useStyles();
 
 	if (isFetching) {
@@ -32,16 +40,5 @@ const TaskCheckbox = React.memo(({ value, disabled, onChange, isFetching }) => {
 		</div>
 	);
 });
-
-TaskCheckbox.propTypes = {
-	value: PropTypes.bool.isRequired,
-	disabled: PropTypes.bool,
-	isFetching: PropTypes.bool,
-	onChange: PropTypes.func.isRequired,
-};
-
-TaskCheckbox.defaultProps = {
-	isFetching: false,
-};
 
 export { TaskCheckbox };
