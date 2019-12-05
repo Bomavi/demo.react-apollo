342ad19cc5566d3b873bb9ca592ac7d9348edaf3
Bomavi <max.bozh.n1mbo@gmail.com>
2019-12-05 23:05:08 +0200

    Switch to TS, add context and apollo

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,25 @@
 /* npm imports: common */
 import React from 'react';
 import { render } from 'react-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
 import { Provider } from 'react-redux';
 
 /* root imports: view components */
 import { App } from 'views/layouts/App';
 
 /* root imports: common */
+import { client } from 'config/apollo';
+import { AppContextProvider } from 'context/app';
 import { store } from 'store';
 
 /* start react app */
 render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
+	<ApolloProvider client={client}>
+		<Provider store={store}>
+			<AppContextProvider>
+				<App />
+			</AppContextProvider>
+		</Provider>
+	</ApolloProvider>,
 	document.getElementById('root')
 );
