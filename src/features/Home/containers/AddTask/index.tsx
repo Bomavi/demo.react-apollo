42af655fe5f0d55cbd409ecd65172f7d45560847
Bomavi <max.bozh.n1mbo@gmail.com>
2019-12-09 20:32:30 +0200

    Add update method for mutation

--- src/features/Home/containers/AddTask/index.tsx
+++ src/features/Home/containers/AddTask/index.tsx
@@ -2,10 +2,18 @@
 import React, { useCallback } from 'react';
 
 /* root imports: graphql */
-import { useCreateTaskMutation } from 'generated/graphql';
+import {
+	useCreateTaskMutation,
+	SearchTasksDocument,
+	SearchTasksQuery,
+	SortDirection,
+} from 'generated/graphql';
 
 /* root imports: view components */
 import { CustomInput } from 'views/elements';
+
+/* root imports: common */
+import { useStore } from 'context';
 
 /* local imports: common */
 // import { useStyles } from './styles';
@@ -13,7 +21,33 @@ import { CustomInput } from 'views/elements';
 const AddTask: React.FC = () => {
 	// const classes = useStyles();
 
-	const [createTask, { loading }] = useCreateTaskMutation();
+	const [{ tasksSortOrder }] = useStore();
+
+	const [createTask, { loading }] = useCreateTaskMutation({
+		update: (cache, { data: createTaskData }) => {
+			const variables = {
+				sortBy: tasksSortOrder,
+			};
+
+			const { searchTasks } = cache.readQuery<SearchTasksQuery>({
+				query: SearchTasksDocument,
+				variables,
+			})!;
+
+			const updatedTasks =
+				tasksSortOrder === SortDirection.Asc
+					? [createTaskData && createTaskData.createTask, ...searchTasks]
+					: [...searchTasks, createTaskData && createTaskData.createTask];
+
+			cache.writeQuery({
+				query: SearchTasksDocument,
+				variables,
+				data: {
+					searchTasks: updatedTasks,
+				},
+			});
+		},
+	});
 
 	const actionHandler = useCallback(
 		value => {
