68d41b85b0a45d2708ac2c5d0446b6be0e73d839
Bomavi <max.bozh.n1mbo@gmail.com>
2019-12-09 20:37:55 +0200

    Switch PasswordInput to TS & add typings

--- src/features/Login/components/PasswordInput/index.tsx
+++ src/features/Login/components/PasswordInput/index.tsx
@@ -1,22 +1,28 @@
 /* npm imports: common */
 import React from 'react';
-import PropTypes from 'prop-types';
 
 /* npm imports: material-ui/core */
-import TextField from '@material-ui/core/TextField';
+import TextField, { TextFieldProps } from '@material-ui/core/TextField';
 
 /* local imports: common */
 // import { useStyles } from './styles';
 
-const PasswordInput = React.memo(({ repeatPassword, ...props }) => {
+export interface PasswordInputProps {
+	repeatPassword?: boolean;
+}
+
+const PasswordInput: React.FC<PasswordInputProps & TextFieldProps> = React.memo(props => {
+	const { repeatPassword, ...restProps } = props;
+
 	// const classes = useStyles();
+
 	const id = repeatPassword ? 'repeat-password' : 'password';
 	const label = repeatPassword ? 'Repeat password' : 'Password';
 
 	return (
 		<TextField
 			fullWidth
-			{...props}
+			{...restProps}
 			id={id}
 			label={label}
 			autoComplete="off"
@@ -26,9 +32,5 @@ const PasswordInput = React.memo(({ repeatPassword, ...props }) => {
 		/>
 	);
 });
-
-PasswordInput.propTypes = {
-	repeatPassword: PropTypes.bool,
-};
 
 export { PasswordInput };
