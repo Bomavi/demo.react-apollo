4623ca773d903abc028e9babb0ea72026e1747da
Bomavi <max.bozh.n1mbo@gmail.com>
2019-12-09 20:30:35 +0200

    Remove redux and add context provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,24 +2,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { Provider } from 'react-redux';
 
 /* root imports: view components */
 import { App } from 'views/layouts/App';
 
 /* root imports: common */
 import { client } from 'config/apollo';
-import { AppContextProvider } from 'context/app';
-import { store } from 'store';
+import { StoreContextProvider } from 'context/store';
 
 /* start react app */
 render(
 	<ApolloProvider client={client}>
-		<Provider store={store}>
-			<AppContextProvider>
-				<App />
-			</AppContextProvider>
-		</Provider>
+		<StoreContextProvider>
+			<App />
+		</StoreContextProvider>
 	</ApolloProvider>,
 	document.getElementById('root')
 );
