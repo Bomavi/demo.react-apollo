97bbe8338120f173d25279b518794814338a877c
Bomavi <max.bozh.n1mbo@gmail.com>
2019-12-09 20:44:02 +0200

    Update layouts/Content for TS

--- src/views/layouts/Content/index.tsx
+++ src/views/layouts/Content/index.tsx
@@ -1,30 +1,30 @@
 /* npm imports: common */
 import React from 'react';
-import { useSelector } from 'react-redux';
+
+/* root imports: graphql */
+import { useCurrentUserQuery } from 'generated/graphql';
 
 /* root imports: view components */
 import { Header, Footer, Drawer } from 'views/layouts';
 
-/* root imports: common */
-import { getIsAuthenticated } from 'selectors';
-
 /* local imports: common */
 import { useStyles } from './styles';
 
-const Content = ({ children }) => {
+const Content: React.FC = React.memo(({ children }) => {
 	const classes = useStyles();
 
-	const isAuthenticated = useSelector(state => getIsAuthenticated(state));
+	const { data: currentUserData } = useCurrentUserQuery();
+	const currentUser = currentUserData ? currentUserData.currentUser : undefined;
 
 	return (
 		<>
 			<Header />
 			<div className={classes.toolbar} />
-			{isAuthenticated && <Drawer />}
+			{currentUser && <Drawer />}
 			<main className={classes.main}>{children}</main>
 			<Footer />
 		</>
 	);
-};
+});
 
 export { Content };
