5d9d92a43f380872ae5285ee78eb435c6e54f57e
Bomavi <max.bozh.n1mbo@gmail.com>
2019-12-10 02:18:49 +0200

    Remove unused mocks

--- src/__mocks__/store/index.js
+++
@@ -1,7 +0,0 @@
-/* npm imports: common */
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-
-const middleware = [thunk];
-
-export const mockStore = configureStore(middleware);
